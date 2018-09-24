b53033d507833683fa907296590eb055735856f0
wduartes <walterduartes@gmail.com>
2018-09-24 11:01:01 -0300

    Aparece el mensaje de sandbox para buscar clinicas por nombre

--- routes/api.php
+++ routes/api.php
@@ -24,17 +24,28 @@ Groups all the message under the version of the api (v1)
 */
 
 Route::group(['prefix'=>'/v1'], function(){
+	//http://laravel.win/api/v1/
+
 	Route::post('/register', 'Auth\RegisterController@register');	
 	Route::post('/login', 'Auth\LoginController@login');	
 	
 	//Groups all message that need an logged user ( with a valid api_token) to work
-	Route::group(['middleware' => 'auth:api'], function(){		
-		Route::post('/logout', 'Auth\LoginController@logout');
-		
+	Route::group(['middleware' => 'auth:api'], function(){	
+
+		Route::post('/logout', 'Auth\LoginController@logout');		
 		Route::post('/deactivate', 'Auth\LoginController@deactivate');
 	
 		Route::get('/user', function(Request $request){
 			return response()->json( Auth::guard('api')->user(), 201);
+		});
+		
+		//Group for all the clinic related messages		
+		Route::group(['prefix'=>'/clinic'], function(){
+			//http://laravel.win/api/v1/clinic
+
+			Route::post('/search', function(Request $request){
+				return response()->json( ["id" => 9999, "name" => "test clinic"], 200);
+			});			
 		});
 	});	
 });
