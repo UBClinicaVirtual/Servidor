5cf4ac6e23e6282946cd37af0460c75f51996f1a
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-09-25 16:17:28 -0300

    Cambio el ver put a post por limitacion del hosting

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ Route::group(['prefix'=>'/v1'], function(){
 		});
 		
 		Route::group(['prefix'=>'/user'], function(){
-			Route::put('/clinic', 'ClinicController@update_profile');
+			Route::post('/clinic', 'ClinicController@update_profile');
 		});
 		
 		//Group for all the clinic related messages		
