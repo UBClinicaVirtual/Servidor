e070a622150d73058a1d8089c86ced89524a1076
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-09-28 19:56:12 -0300

    Aparece el campo id para los HCP

--- app/HCP.php
+++ app/HCP.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class HCP extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 
+	];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
 }
