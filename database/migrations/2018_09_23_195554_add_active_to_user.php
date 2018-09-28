52e8df6935a26118448382299bc476c81447105c
PedroRodriguez95 <pedro.rodriguez@comunidad.ub.edu.ar>
2018-09-28 20:27:11 -0300

    Se realiza la migracion de la tabla ususarios y se configura SQLite para guardar en memoria

--- database/migrations/2018_09_23_195554_add_active_to_user.php
+++ database/migrations/2018_09_23_195554_add_active_to_user.php
@@ -14,7 +14,7 @@ class AddActiveToUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('active');
+            $table->integer('active')->default(0);
         });
     }
 
