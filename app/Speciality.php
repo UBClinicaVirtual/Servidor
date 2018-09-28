ae88a654c919450f37cf2991dd63ce435fa3e2f4
wduartes <walterduartes@gmail.com>
2018-09-28 09:55:20 -0300

    Se agregan los campos al modelo de Speciality

--- app/Speciality.php
+++ app/Speciality.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Speciality extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'active',
+	];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
 }
