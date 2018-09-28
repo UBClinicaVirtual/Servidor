6fe4cd4587c55c58bf6a34923edc7eb4648e8955
wduartes <walterduartes@gmail.com>
2018-09-28 14:18:11 -0300

    Se agregan los validators de request para crear,modificar y buscar especialidades

--- app/Http/Controllers/SpecialityController.php
+++ app/Http/Controllers/SpecialityController.php
@@ -30,8 +30,43 @@ class SpecialityController extends Controller
 		$this->middleware('auth:api');
     }
 	
+	protected function validateCreationRequest( Request $request )
+	{
+		return Validator::make(	$request->all(), 
+								[
+									"name" => "required|string|min:3"
+								]		
+								);		
+	}	
+	
+	protected function validateUpdateRequest( Request $request )
+	{
+		return Validator::make(	$request->all(), 
+								[
+									"name" => "required|string|min:3",
+									"active" => "required|integer",
+								]		
+								);		
+	}	
+	
+	protected function validateSearchRequest( Request $request )
+	{
+		return Validator::make(	$request->all(), 
+								[
+									"name" => "string|min:3",
+									"active" => "integer",
+								]		
+								);		
+	}
+	
 	public function create(Request $request)
 	{
+		//get the validator for the creation
+		$validator = $this->validateCreationRequest( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+			
 		$speciality = Speciality::create(["name" => $request["name"]]);	
 		return response()->json( $speciality, 201);
 	}	
@@ -42,8 +77,16 @@ class SpecialityController extends Controller
 	}
 	
 	public function update(Request $request, Speciality $speciality)
-	{
-		$speciality->name = $request["name"];
+	{	
+		//get the validator for the update of a Speciality
+		$validator = $this->validateUpdateRequest( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+			
+		$speciality->name = $request["name"];		
+		$speciality->active = $request["active"];
+		
 		$speciality->save();
 		
 		return response()->json( $speciality, 201);
@@ -51,6 +94,12 @@ class SpecialityController extends Controller
 	
 	public function search(Request $request)
 	{
+		//get the validator for the update of a Speciality
+		$validator = $this->validateSearchRequest( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+			
 		return response()->json( ["specialities" => Speciality::active()->get() ], 201);
 	}	
 }
