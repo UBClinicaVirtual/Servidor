e82923f18fe233c9b91ecab2059f7afe39bf23ae
wduartes <walterduartes@gmail.com>
2018-10-02 11:59:04 -0300

    Se agrega la validacion para agregar el perfil de paciente

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -33,10 +33,29 @@ class PatientController extends Controller
 	public function search(Request $request )
 	{			
 		return response()->json( [ "msg" => "unimplemented method" ], 403);
-	}			
+	}
+
+	/*
+	| validate the request has at least a name and a identification number ( DNI )
+	*/
+	protected function validateProfileRequest( Request $request )
+	{
+		return Validator::make(	$request->all(), 
+								[
+									"name" => "required|string|min:3",
+									"identification_number" => "required|string|min:3",
+								]		
+								);		
+	}
 	
 	public function update_profile(Request $request )
 	{
+		//get the validator for the creation
+		$validator = $this->validateProfileRequest( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+			
 		$user = Auth::guard('api')->user();
 		
 		$patient = Patient::where( 'id', $user->id )->first();
