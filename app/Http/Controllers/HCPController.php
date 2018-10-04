7fdb4598f6c736267b4736a1d7a076cebd205c5b
wduartes <walterduartes@gmail.com>
2018-10-04 09:57:36 -0300

    Aparece el metodo para obtener los turnos del perfil de medico del usuario

--- app/Http/Controllers/HCPController.php
+++ app/Http/Controllers/HCPController.php
@@ -106,4 +106,33 @@ class HCPController extends Controller
 			
 		return response()->json([ 'hcp' => ['hcp' => $hcp, 'specialities' => $hcp->specialities()->get() ] ], 201);
 	}
+	
+	public function search_appointments( Request $request){
+		return response()->json(['appointments' => [ [ 	
+														"id_appointment" => 753, 
+														"id_clinic" => 123, 
+														"clinic_name" => "Clinica de la trinidad",
+														"id_speciality" => 789, 
+														"speciality_name" => "Guardia de ginecologia",
+														"id_patient" => 1425, 
+														"patient_name" => "Jesus de Nazaret",
+														"appointment_date" => "2018/01/02 12:57",
+														"appointment_state" => 1,
+														"appointment_state_label" => "Pending",
+														],
+														[ 	
+														"id_appointment" => 8820, 
+														"id_clinic" => 123, 
+														"clinic_name" => "Clinica de la trinidad",
+														"id_speciality" => 124, 
+														"speciality_name" => "Traumatologo",
+														"id_patient" => 1024, 
+														"id_patient" => "Garcia Marquez",
+														"appointment_date" => "2018/04/01 16:90",
+														"appointment_state" => 1,
+														"appointment_state_label" => "Pending",
+														],
+														
+													] ], 200);
+	}
 }
