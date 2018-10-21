3c4fe5311ff765647fd7715c1ec71786f7d0735f
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-10-21 11:26:18 -0300

    Se agrega el metodo get_profile a User para obtener los distintos perfiles de usuario

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('guest')->except('logout');
         $this->middleware('guest', ['except' => ['logout','deactivate']]);
     }
 	
@@ -58,17 +57,13 @@ class LoginController extends Controller
 		// calls to the laravel default function to login
 		// It ll create a new api_token for each call to login
 		// ill send the gmail mail only to the laravel login function
-//		if ($this->attemptLogin($request)) {
-//		if ($this->attemptLogin( $request->only('email') ) ) {
+		
 		if ($this->guard()->attempt( ['email' => $userInfo['email'] ] ) ) {
 		
 			$user = $this->guard()->user();
 			$user->generateToken();
 
-			return response()->json([
-				'data' => $user->toArray(),
-				'gmail' => $userInfo,
-			]);
+			return response()->json( $user->get_profile() );
 		}
 	
 		return $this->sendFailedLoginResponse($request);
