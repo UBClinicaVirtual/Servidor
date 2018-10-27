7aa33e7a47ca694973fa6bad3a0fc0cdcd6a38ae
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-10-26 21:11:26 -0300

    Se agrega el metodo para poder buscar turnos por filtro

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,13 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-
+use App\Searchers\AppointmentSearch\AppointmentSearch as AppointmentSearch;
 use App\AppointmentStatus;
 
 class AppointmentController extends Controller
 {
+	protected function validateRequestAppointmentSearch(Request $request)
+	{
+		/*
+			TODO: this should have in consideration searching by many another fields
+			like HCP Speciality, appointment disponibility, etc.
+		*/
+		
+		return Validator::make(	$request->all(), 
+								[
+									"patient_id" => "integer",
+									"hcp_id" => "integer",
+									"clinic_id" => "integer",
+								]		
+								);
+	}
+	
     static public function search(Request $request)
 	{
+		//get the validator for the search
+		$validator = $this->validateRequestAppointmentSearch( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+
+		//Get all the records that match the filter sent		
+		return AppointmentSearch::apply( $request );
+		
+/*		
 		$appointments = DB::table('appointments')
 						->select(
 						'appointments.id',
@@ -34,6 +60,7 @@ class AppointmentController extends Controller
 						->get();
 		
 		return $appointments;				
+*/		
 	}
 	
 	public function all_status(Request $request)
