24db7c6173ceb37d74f35cfee4992e749372531d
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-10-26 21:52:35 -0300

    Se cambian los filtros para que usen QueryBuilder en lugar de EloquentBuilder

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
@@ -10,7 +11,7 @@ use App\AppointmentStatus;
 
 class AppointmentController extends Controller
 {
-	protected function validateRequestAppointmentSearch(Request $request)
+	static protected function validateRequestAppointmentSearch(Request $request)
 	{
 		/*
 			TODO: this should have in consideration searching by many another fields
@@ -29,7 +30,7 @@ class AppointmentController extends Controller
     static public function search(Request $request)
 	{
 		//get the validator for the search
-		$validator = $this->validateRequestAppointmentSearch( $request );
+		$validator = static::validateRequestAppointmentSearch( $request );
 		
 		if( $validator->fails() ) 
 			return response()->json( [ "msg" => $validator->errors() ], 403);
