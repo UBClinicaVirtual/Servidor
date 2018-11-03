412a6e31ca4f752bf9abba73fd1728903f9b3cb3
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-11-03 11:44:59 -0300

    Se modifica el metodo get_profile para obtener un id de tipo de usuario

--- app/User.php
+++ app/User.php
@@ -9,7 +9,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+	
+	// Ids of user types of the user profile
+	const USER_TYPE_NEW = 0;
+	const USER_TYPE_PATIENT = 1;
+	const USER_TYPE_HCP = 2;
+	const USER_TYPE_CLINIC = 3;
+	
+	
     /**
      * The attributes that are mass assignable.
      *
@@ -70,16 +77,24 @@ class User extends Authenticatable
 		$profile = array();
 		
 		$profile = array_merge($profile, [ "user" => $this ] );
+		$user_type_id = self::USER_TYPE_NEW;
 		
-		if( $this->patient()->exists() )
+		if( $this->patient()->exists() ){
+			$user_type_id = self::USER_TYPE_PATIENT;
 			$profile = array_merge( $profile, [ "patient" => $this->patient()->first() ] );
-
-		if( $this->hcp()->exists() )
+		}
+		
+		if( $this->hcp()->exists() ){
+			$user_type_id = self::USER_TYPE_HCP;
 			$profile = array_merge( $profile, [ "hcp" => $this->hcp()->first() ] );
+		}
 		
-		if( $this->clinic()->exists() )
+		if( $this->clinic()->exists() ){
+			$user_type_id = self::USER_TYPE_CLINIC;
 			$profile = array_merge( $profile, [ "clinic" => $this->clinic()->first() ] );
+		}
 		
+		$profile['user']['user_type_id'] = $user_type_id;
 		return $profile;
 	}
 }
