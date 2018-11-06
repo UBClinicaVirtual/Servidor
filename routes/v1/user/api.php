a77ca9530253e8bc7d57d4be8f503f8e01c5dc96
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-11-05 22:52:24 -0300

    Se agregan las routes para poder buscar registros medicos por clinica o medico

--- routes/v1/user/api.php
+++ routes/v1/user/api.php
@@ -19,6 +19,7 @@
 			});
 			
 			Route::post('/appointments', 'ClinicController@search_appointments');
+			Route::post('/medical_records', 'ClinicController@search_medical_records');
 		});
 		
 		Route::group(['prefix'=>'/patient'], function(){
@@ -34,6 +35,7 @@
 			Route::post('', 'HCPController@update_profile');
 			
 			Route::post('/appointments', 'HCPController@search_appointments');
+			Route::post('/medical_records', 'HCPController@search_medical_records');
 		});
 	});
 ?>
