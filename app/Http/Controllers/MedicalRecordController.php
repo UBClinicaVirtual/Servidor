67e84487bb4121f7387ee351710890a040928748
WalterDuartesUB <walter.duartes@comunidad.ub.edu.ar>
2018-11-05 22:30:30 -0300

    Se agrega el metodo para poder buscar los registros medicos

--- app/Http/Controllers/MedicalRecordController.php
+++ app/Http/Controllers/MedicalRecordController.php
@@ -7,5 +7,26 @@ use App\Http\Controllers\Controller;
 
 class MedicalRecordController extends Controller
 {
-    //
+	static protected function validateRequestMedicalRecordSearch(Request $request)
+	{		
+		return Validator::make(	$request->all(), 
+								[
+									"patient_id" => "integer",
+									"hcp_id" => "integer",
+									"clinic_id" => "integer",
+								]		
+								);
+	}
+	
+    static public function search(Request $request)
+	{
+		//get the validator for the search
+		$validator = static::validateRequestMedicalRecordSearch( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+
+		//Get all the records that match the filter sent		
+		return MedicalRecordSearch::apply( $request );	
+	}
 }
