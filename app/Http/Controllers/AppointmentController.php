3626a19e7b026c14d9aa87226ca7a9169063b6cd
wduartes <walterduartes@gmail.com>
2018-11-07 10:05:44 -0300

    Se remueve comentario del codigo

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -36,32 +36,7 @@ class AppointmentController extends Controller
 			return response()->json( [ "msg" => $validator->errors() ], 403);
 
 		//Get all the records that match the filter sent		
-		return AppointmentSearch::apply( $request );
-		
-/*		
-		$appointments = DB::table('appointments')
-						->select(
-						'appointments.id',
-						'hcps.id as hcp_id', 'hcps.first_name as hcp_first_name', 'hcps.last_name as hcp_last_name', 
-						'clinics.id as clinic_id', 'clinics.business_name as clinic_business_name', 
-						'specialities.id as speciality_id', 'specialities.name as speciality_name', 
-						'patients.id as patient_id', 'patients.first_name as patient_first_name', 'patients.last_name as patient_last_name', 
-						'appointment_date',
-						'appointment_status_id',
-						'appointment_statuses.name as appointment_status_name'
-						)						
-						->join('clinic_appointment_schedule', 'clinic_appointment_schedule.id', '=', 'appointments.clinic_appointment_schedule_id')
-						->join('clinic_hcp_specialities', 'clinic_hcp_specialities.id', '=', 'clinic_appointment_schedule.clinic_hcp_speciality_id' )
-						->join('clinics', 'clinics.id', '=', 'clinic_hcp_specialities.clinic_id')
-						->join('hcp_specialities', 'hcp_specialities.id', '=',  'clinic_hcp_specialities.hcp_speciality_id')
-						->join('hcps', 'hcps.id', '=', 'hcp_specialities.hcp_id')
-						->join('specialities', 'specialities.id','=','hcp_specialities.speciality_id')
-						->join('patients', 'patients.id','=','appointments.patient_id')
-						->join('appointment_statuses', 'appointment_statuses.id','=','appointments.appointment_status_id')
-						->get();
-		
-		return $appointments;				
-*/		
+		return AppointmentSearch::apply( $request );	
 	}
 	
 	public function all_status(Request $request)
