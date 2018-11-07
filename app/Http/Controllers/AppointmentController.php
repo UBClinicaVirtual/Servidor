8a25e5beed75776ea8e309ab5c0369f73936c419
wduartes <walterduartes@gmail.com>
2018-11-07 10:09:34 -0300

    Se agrega el metodo para encontrar los turnos disponibles a ser tomados

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Searchers\AppointmentSearch\AppointmentSearch as AppointmentSearch;
+use App\Searchers\AppointmentAvailableSearch\AppointmentAvailableSearch as AppointmentAvailableSearch;
 use App\AppointmentStatus;
 
 class AppointmentController extends Controller
@@ -26,6 +27,23 @@ class AppointmentController extends Controller
 								]		
 								);
 	}
+
+	static protected function validateRequestAppointmentAvailableSearch(Request $request)
+	{
+		/*
+			TODO: this should have in consideration searching by many another fields
+			like HCP Speciality, appointment disponibility, etc.
+		*/
+		
+		return Validator::make(	$request->all(), 
+								[
+									"hcp_id" => "required|integer",
+									"clinic_id" => "required|integer",
+									"date_from" => "required|date|date_format:Y-m-d",
+									"date_to" => "required|date|date_format:Y-m-d",
+								]		
+								);
+	}
 	
     static public function search(Request $request)
 	{
@@ -37,6 +55,18 @@ class AppointmentController extends Controller
 
 		//Get all the records that match the filter sent		
 		return AppointmentSearch::apply( $request );	
+	}	
+	
+    static public function search_available(Request $request)
+	{
+		//get the validator for the search
+		$validator = static::validateRequestAppointmentAvailableSearch( $request );
+		
+		if( $validator->fails() ) 
+			return response()->json( [ "msg" => $validator->errors() ], 403);
+
+		//Get all the records that match the filter sent		
+		return AppointmentAvailableSearch::apply( $request );	
 	}
 	
 	public function all_status(Request $request)
