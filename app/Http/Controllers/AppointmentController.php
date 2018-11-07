fbc1d83aa2c0816ac80337c20c86b43d2509d15c
wduartes <walterduartes@gmail.com>
2018-11-07 10:42:31 -0300

    Se agrega el llamado para obtenr el calendario

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use App\Searchers\AppointmentSearch\AppointmentSearch as AppointmentSearch;
-use App\Searchers\AppointmentAvailableSearch\AppointmentAvailableSearch as AppointmentAvailableSearch;
+
 use App\AppointmentStatus;
 
 class AppointmentController extends Controller
 {
+	const APPOINTMENT_PENDING = 1;
+	const APPOINTMENT_COMPLETE = 3;
+	
 	static protected function validateRequestAppointmentSearch(Request $request)
 	{
 		/*
@@ -65,8 +68,35 @@ class AppointmentController extends Controller
 		if( $validator->fails() ) 
 			return response()->json( [ "msg" => $validator->errors() ], 403);
 
-		//Get all the records that match the filter sent		
-		return AppointmentAvailableSearch::apply( $request );	
+		//Adds the day of the week to the criteria
+		$request["day_of_the_week"] = [ min( date('N', $request["date_from"]), date('N', $request["date_to"]) ), max( date('N', $request["date_from"]), date('N', $request["date_to"]) ) ];
+		
+		//Get the schedule within the criteria
+		$schedule = ScheduleSearch::apply( $request );	
+		
+		return $schedule;
+/*		
+		$appointment_filter = [
+			"clinic_id" => $request["clinic_id"],
+			"hcp_id" => $request["hcp_id"],
+			"status" => [ self::APPOINTMENT_PENDING, self::APPONTMENT_COMPLETE ],
+			"date_range" => [ $request["date_from"], $request["date_to"] ],
+			"schedule_ids" => array_map(create_function('$o', 'return $o->id;'), $schedule)
+		];
+		
+		return $appointment_filter;
+		
+		//Gets the taken appointments
+		$taken_appointments = AppointmentSearch::apply([
+			"clinic_id" => $request["clinic_id"],
+			"hcp_id" => $request["hcp_id"],
+			"status" => [ self::APPOINTMENT_PENDING, self::APPONTMENT_COMPLETE ],
+			"date_range" => [ $request["date_from"], $request["date_to"] ],
+			"schedule_ids" => array_map(create_function('$o', 'return $o->id;'), $schedule)
+		]);
+
+		return $taken_appointments;
+*/		
 	}
 	
 	public function all_status(Request $request)
