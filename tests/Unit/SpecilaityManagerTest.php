8feb14f4b03a4c1f55f30f8c3cd828092e65e4da
PedroRodriguez95 <Pedro.rodiguez@comunidad.ub.edu.ar>
2018-11-12 19:47:42 -0300

    Se crean las pruebas para el metodo search del SpecialityManager

--- tests/Unit/SpecilaityManagerTest.php
+++ tests/Unit/SpecilaityManagerTest.php
@@ -14,89 +14,124 @@ require './vendor/autoload.php';
 
 class SpecialityManagerTest extends TestCase
 {
+    protected $manager;
 
     public function setUp()
     {
         parent::setUp();
         Artisan::call('migrate:refresh');
+        $this->manager = new SpecialityManager();
     }
 
     private function invalidData(){
         return array('name' => 'as');
     }
 
-        private function validData(){
+    private function validData(){
         return array('name' => 'asd');
     }
 
 
     function test_ManagerSpecialty_Create_With_Invalid_Input() {
-        $manager = new SpecialityManager();
-        $response = $manager->create($this->invalidData());
+
+        $response = $this->manager->create($this->invalidData());
         $this->assertEquals(403, $response->getStatusCode());
 
     }
 
     function test_ManagerSpecialty_Create_With_Valid_Input() {
-        $manager = new SpecialityManager();
-        $response = $manager->create($this->validData());
+
+        $response = $this->manager->create($this->validData());
         $this->assertEquals(201, $response->getStatusCode());
 
     }
 
     function test_ManagerSpecialty_Create_Returns_Correct_Data_Structure() {
-        $manager = new SpecialityManager();
-        $response = $manager->create($this->validData());
+
+        $response = $this->manager->create($this->validData());
         $content = json_decode($response->getContent(), true);
         $this->assertArrayHasKey('name', $content);
         $this->assertArrayHasKey('id', $content);
+
     }
 
     function test_ManagerSpecialty_Create_Returns_Correct_Data() {
-        $manager = new SpecialityManager();
-        $response = $manager->create($this->validData());
+
+        $response = $this->manager->create($this->validData());
         $content = json_decode($response->getContent(), true);
         $this -> assertEquals($this->validData()['name'], $content['name'] );
 
     }
 
     function test_ManagerSpecialty_Update_With_Invalid_Input() {
+
         $speciality = new Speciality;
         $speciality->fill(array('name' => 'test'));
-        $manager = new SpecialityManager();
-        $response = $manager->update($this->invalidData(), $speciality);
+        $response = $this->manager->update($this->invalidData(), $speciality);
         $this->assertEquals(403, $response->getStatusCode());
 
     }
 
-    function test_ManagerSpecialty_Update_With_valid_Input() {
+    function test_ManagerSpecialty_Update_With_Valid_Input() {
+
         $speciality = new Speciality;
         $speciality->fill(array('name' => 'test'));
-        $manager = new SpecialityManager();
-        $response = $manager->update($this->validData(), $speciality);
+        $response = $this->manager->update($this->validData(), $speciality);
         $this->assertEquals(201, $response->getStatusCode());
 
     }
 
-    function test_ManagerSpecialty_Returns_Correct_Data_Structure() {
+    function test_ManagerSpecialty_Update_Returns_Correct_Data_Structure() {
+
         $speciality = new Speciality;
         $speciality->fill(array('name' => 'test'));
-        $manager = new SpecialityManager();
-        $response = $manager->update($this->validData(), $speciality);
+        $response = $this->manager->update($this->validData(), $speciality);
         $content = json_decode($response->getContent(), true);
         $this->assertArrayHasKey('name', $content);
         $this->assertArrayHasKey('id', $content);
 
     }
 
-    function test_ManagerSpecialty_Returns_Correct_Data() {
+    function test_ManagerSpecialty_Update_Returns_Correct_Data() {
+
         $speciality = new Speciality;
         $speciality->fill(array('name' => 'test'));
-        $manager = new SpecialityManager();
-        $response = $manager->update($this->validData(), $speciality);
+        $response = $this->manager->update($this->validData(), $speciality);
         $content = json_decode($response->getContent(), true);
         $this->assertEquals($this->validData()['name'], $content['name']);
 
+    }
+
+    function test_ManagerSpecialty_Search_With_Invalid_Data() {
+
+        $this->manager->create($this->validData());
+        $response = $this->manager->search($this->invalidData());
+        $content = json_decode($response->getContent(), true);
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    function test_ManagerSpecialty_Search_With_valid_Data() {
+
+        $this->manager->create($this->validData());
+        $response = $this->manager->search($this->validData());
+        $content = json_decode($response->getContent(), true);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    function test_ManagerSpecialty_Search_Returns_Correct_Data_Structure() {
+
+        $this->manager->create($this->validData());
+        $response = $this->manager->search($this->validData());
+        $content = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('specialities', $content);
+    }
+
+    function test_ManagerSpecialty_Search_Returns_Correct_Data() {
+        
+        $this->manager->create($this->validData());
+        $response = $this->manager->search($this->validData());
+        $content = json_decode($response->getContent(), true);
+        $this->assertEquals($this->validData()['name'], $content['specialities'][0]['name']);
     }
 
 }
